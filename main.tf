d2ac0e538f3bf2690887c8b210e7c039dd186d02
bilooaa <ashish_bhargava85@yahoo.com>
2024-11-18 14:24:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "vpc" {
 
 module "subnet" {
   source                  = "./modules/subnet"
-  vpc_id                  = module.vpc.vpc_id
+  vpc_id                  = modules.vpc.vpc_id
   subnet_name             = "my-subnet"
   subnet_cidr             = "10.0.1.0/24"
   map_public_ip_on_launch = true
